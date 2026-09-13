10ae29914bb16688a2fedb762c4b2cf377559fd6
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2026-09-13 12:01:43 +0700

    init

--- books_data.js
+++ books_data.js
@@ -7,6 +7,20 @@ const BOOKS_DATA = [
     "dateAdded": "Gần đây"
   },
   {
+    "title": "A Trader’s Money Management System — Bennett A. McDowell",
+    "filename": "books/money-management-mcdowell.html",
+    "category": "Đầu tư & Trading",
+    "author": "Chuyên gia / Tác giả",
+    "dateAdded": "Gần đây"
+  },
+  {
+    "title": "Tóm Tắt Chiến Lược: Cách Kiếm Lợi Nhuận 18.000% — Gil Morales & Chris Kacher",
+    "filename": "books/trade-like-oneil-disciple.html",
+    "category": "Đầu tư & Trading",
+    "author": "Chuyên gia / Tác giả",
+    "dateAdded": "Gần đây"
+  },
+  {
     "title": "Wyckoff 2.0 — Rubén Villahermosa Chaves",
     "filename": "books/wyckoff-2-0-summary.html",
     "category": "Đầu tư & Trading",
